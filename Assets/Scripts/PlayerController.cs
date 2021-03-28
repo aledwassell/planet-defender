310ba1014dabe21f64f1d34edd57cc979b657e9c
aledwassell <aledwassell@googlemail.com>
2021-03-28 17:00:53 +0100

    upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,22 @@ public class PlayerController : MonoBehaviour
         rb.useGravity = false;
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         float mHorizontal = Input.GetAxis("Horizontal");
-        // float mVertical = Input.GetAxis("Vertical");
+        float mVertical = Input.GetAxis("Vertical");
 
         Vector3 move3d = new Vector3(mHorizontal, 0, 0).normalized;
 
         rb.MovePosition(rb.position + transform.TransformDirection(move3d) * playerSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // We don't want the player geometry to move, so ignore collisions from trees and rocks.
+        if (other.gameObject.tag != "Planet")
+        {
+            Physics.IgnoreCollision(other.collider, GetComponent<Collider>());
+        }
     }
 }
---
+++ Assets/Scripts/SpawnFauna.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class used to sqawn the trees and rocks.
+public class SpawnFauna : MonoBehaviour
+{
+    public GameObject[] faunaPrefabs;
+    public int spawnCount = 150;
+
+    void Start()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 dir = Random.onUnitSphere;
+            if (dir.z <= 0) SpawnObj(dir);
+        }
+
+    }
+
+    void SpawnObj(Vector3 dir)
+    {
+        int faunaID = Random.Range(0, faunaPrefabs.Length);
+        Vector3 spawnPoint = transform.position;
+        spawnPoint += dir * transform.localScale.y / 2;
+        Vector3 spawnRotation = (spawnPoint - transform.position).normalized;
+        GameObject addFauna = Instantiate(faunaPrefabs[faunaID], spawnPoint, new Quaternion(0, 0, 0, 0));
+        addFauna.transform.rotation = Quaternion.FromToRotation(addFauna.transform.up, spawnRotation) * addFauna.transform.rotation;
+    }
+}
