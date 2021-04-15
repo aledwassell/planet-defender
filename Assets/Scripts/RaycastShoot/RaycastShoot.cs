8d235e7dc90fbfae204728f5946c979c686eea01
aledwassell <aledwassell@googlemail.com>
2021-04-15 07:42:00 +0100

    initial commit

---
+++ Assets/Scripts/RaycastShoot/RaycastShoot.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour
+{
+    public int gunDamage = 1;                                            // Set the number of hitpoints that this gun will take away from shot objects with a health script
+    public float fireRate = 0.25f;                                        // Number in seconds which controls how often the player can fire
+    public float weaponRange = 50f;                                        // Distance in Unity units over which the player can fire
+    public float hitForce = 100f;                                        // Amount of force which will be added to objects with a rigidbody shot by the player
+    public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
+    private AudioSource gunAudio;                                        // Reference to the audio source which will play our shooting sound effect
+    private LineRenderer laserLine;                                        // Reference to the LineRenderer component which will display our laserline
+    private float nextFire;                                                // Float to store the time the player will be allowed to fire again, after firing
+    
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOigin
+        }
+
+    }
+
+    IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
